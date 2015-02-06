7516548d015940f5e3872086e61f6dcf6a7869fb
Amitava Saha <amitava82@gmail.com>
2015-02-06 16:19:55 +0530

    fixed #44

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['ui.multiselect'])
 
 .controller('appCtrl', ['$scope', function($scope){
-    $scope.cars = [{id:1, name: 'Audi'}, {id:2, name: 'BMW'}, {id:1, name: 'Honda'}];
+    $scope.cars = [{id:1, name: 'Audi'}, {id:2, name: 'BMW'}, {id:3, name: 'Honda'}];
     $scope.selectedCar = [];
 
     $scope.fruits = [{id: 1, name: 'Apple'}, {id: 2, name: 'Orange'},{id: 3, name: 'Banana'}];
