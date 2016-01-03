d4e1799664d73e394d8be615253d6a40907dca24
Arindrajit Biswas <arindrajitb@gmail.com>
2016-01-03 16:18:01 +0000

    code merged from https://github.com/zachlysobey/angular-multiselect and https://github.com/amitava82/angular-multiselect . new functionality added 1) added tracked by id or $index. id can be spefied as attribute. Css can be applied to while list as well as list item. example updaed to show both scenario. example updaed to show object selection.

--- dist/app.js
+++ dist/app.js
@@ -4,14 +4,16 @@ angular.module('app', ['am.multiselect'])
     $scope.cars = [
                     {id:1, name: 'Audi'},
                     {id:2, name: 'BMW'},
-                    {id:3, name: 'Honda'}
+                    {id:3, name: 'Honda'},
+                    {id:4, name: 'A very very long name with spaces. It never ends at all. Keeps going on and on and on. So what can you do.'},
+                    {id:5, name: 'AndThereComesTheNoSpaceMonsterThatYouHateToSeeInYourWebPageWhatCanYouDoAboutThis'}
                 ];
     $scope.selectedCar = [];
 
     $scope.fruits = [
                         {id: 1, name: 'Apple'},
                         {id: 2, name: 'Orange'},
-                        {id: 3, name: 'Banana'}
+                        {id: 3, name: 'Banana: with some long story like there usual travel across the atlantic from south worm world to cold dizzy northern world.'}
                     ];
     $scope.selectedFruit = null;
 }]);
