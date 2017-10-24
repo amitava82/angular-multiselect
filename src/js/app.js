e91bd31076bfcb1765b009a4e5cbea600de96e42
NithinBiliya <nithin.biliya@awpl.co>
2017-10-24 14:22:47 +0530

    fixed css - v1.3.8

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,66 @@ angular.module('app', ['am.multiselect'])
       {
         id: 3,
         name: 'Honda'
+      },
+      {
+        id: 4,
+        name: 'Audi'
+      },
+      {
+        id: 5,
+        name: 'BMW'
+      },
+      {
+        id: 6,
+        name: 'Honda'
+      },
+      {
+        id: 7,
+        name: 'Audi'
+      },
+      {
+        id: 8,
+        name: 'BMW'
+      },
+      {
+        id: 9,
+        name: 'Honda'
+      },
+      {
+        id: 10,
+        name: 'Audi'
+      },
+      {
+        id: 11,
+        name: 'BMW'
+      },
+      {
+        id: 12,
+        name: 'Honda'
+      },
+      {
+        id: 13,
+        name: 'Audi'
+      },
+      {
+        id: 14,
+        name: 'BMW'
+      },
+      {
+        id: 15,
+        name: 'Honda'
+      },
+      {
+        id: 16,
+        name: 'Audi'
+      },
+      {
+        id: 17,
+        name: 'BMW'
+      },
+      {
+        id: 18,
+        name: 'Honda'
       }
                 ];
     vm.selectedCar = [];
